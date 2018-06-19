626ef0e177b9a4d3551f7ac0a58589d7978d2f26
splinterific <markgpreston@gmail.com>
2018-06-19 12:30:41 +0100

    updating config

--- config.py
+++ config.py
@@ -9,14 +9,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     VERIFICATION_TOKEN = os.environ.get(
-        "SO_VERIFICATION_TOKEN") or local_config.veri
-    BOT_TOKEN = os.environ.get("GB_TOKEN") or local_config.bot_token
-    USER_TOKEN = os.environ.get("GB_USER_TOKEN") or local_config.user_token
-    OAUTH_SCOPE = os.environ.get("SCOPES") or local_config.scopes
-    CLIENT_ID = os.environ.get("CLIENT_ID") or local_config.client_id
+        "SO_VERIFICATION_TOKEN")
+    BOT_TOKEN = os.environ.get("GB_TOKEN")
+    USER_TOKEN = os.environ.get("GB_USER_TOKEN")
+    OAUTH_SCOPE = os.environ.get("SCOPES")
+    CLIENT_ID = os.environ.get("CLIENT_ID")
     CLIENT_SECRET = os.environ.get(
-        "CLIENT_SECRET") or local_config.client_secret
+        "CLIENT_SECRET")
     CELERY_BROKER_URL = os.environ.get(
-        "REDIS_URL") or local_config.celery_broker_url
+        "REDIS_URL")
     CELERY_RESULT_BACKEND = os.environ.get(
-        "REDIS_URL") or local_config.celery_url_results
+        "REDIS_URL")
