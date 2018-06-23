07376254c0ce4db43e91ed054e6560417cb7114b
splinterific <markgpreston@gmail.com>
2018-06-23 09:10:44 +0100

    cleaned up env files, fixed celery worker, fixed imports

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
 import os
-try:
-    import local_config
-except:
-    pass
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
